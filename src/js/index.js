e9c2a5f9f41d5fad99d290ef5d7accc41b159ddb
Andrey Morozov <am@norse.digital>
2018-11-16 10:18:50 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,7 @@ require("jquery-validation");
 //import popper from 'popper.js';
 //import bootstrap from 'bootstrap';
 
-
-
 $(function() {
-    //jQuery('body').css('color', 'red');
-    console.log('working');
 
     $('.menu-toggle').click(function(){
 		$(this).toggleClass('active');
@@ -46,6 +42,56 @@ $(function() {
     $("#first-try-form").validate();
     $("#second-try-form").validate();
     $("#popup-try-form").validate();
+
+    $('#first-try-form input').bind('keyup blur click', function () { 
+        if ($('#first-try-form').validate().checkForm()) {                   
+            $('#first-try-submit').removeClass('button_disabled').prop('disabled', false); 
+        } else {
+            $('#first-try-submit').addClass('button_disabled').prop('disabled', true);
+        }
+    });
+
+    $('#second-try-form input').bind('keyup blur click', function () { 
+        if ($('#second-try-form').validate().checkForm()) {                   
+            $('#second-try-submit').removeClass('button_disabled').prop('disabled', false); 
+        } else {
+            $('#second-try-submit').addClass('button_disabled').prop('disabled', true);
+        }
+    });
+
+    $('#popup-try-form input').bind('keyup blur click', function () { 
+        if ($('#popup-try-form').validate().checkForm()) {                   
+            $('#popup-try-submit').removeClass('button_disabled').prop('disabled', false); 
+        } else {
+            $('#popup-try-submit').addClass('button_disabled').prop('disabled', true);
+        }
+    });
+
+    $('.sendForm').on('submit', function(e){
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            type: $(this).attr('method'),
+            success: function(data) {
+                if(data.status == 'ok') {
+                    console.log('submitted');
+                    $.fancybox.close();
+                    $.fancybox.open({ 
+                        src: '#thanks',
+                        baseClass: 'popup-transparent'
+                    });
+                } else {
+                    console.log('fail');
+                }
+            },
+            error: function() {
+                console.log('error');
+                $.fancybox.open('<div class="message"><h2>Oops!</h2><p>Something went wrong</p></div>');
+            }
+        });
+    });
 
     
 
