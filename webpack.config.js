e65f5771fccee4dd47ad78d4555ff6b80125c876
Andrey Morozov <am@norse.digital>
2018-11-17 09:21:18 +0200

    lottie refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -101,6 +101,10 @@ module.exports = {
       {
         from: './src/images',
         to: './images'
+      },
+      {
+        from: './src/js/lottie.js',
+        to: './js/lottie.js'
       }
     ]),
   ].concat(htmlPlugins)
