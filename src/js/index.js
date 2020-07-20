83693ec33aca8aca246a1ee9cd7c55cd53eb0d16
Andrey Morozov <am@norse.digital>
2020-07-20 17:35:54 +0300

    menu toggle

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,12 @@ console.log('it works');
 
 const _body = document.body;
 const toggle = document.getElementById('mobile-menu');
+const toggleClose = document.getElementById('mobile-menu-close');
 
 toggle.addEventListener('click', () => {
+  _body.toggleAttribute('data-menu');
+});
+
+toggleClose.addEventListener('click', () => {
   _body.toggleAttribute('data-menu');
 });
