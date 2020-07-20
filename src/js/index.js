4bdac70dc2c8ac9eda485f1779fe2ded7a7f7275
Andrey Morozov <am@norse.digital>
2020-07-20 16:54:32 +0300

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 console.log('it works');
+
+const _body = document.body;
+const toggle = document.getElementById('mobile-menu');
+
+toggle.addEventListener('click', () => {
+  _body.toggleAttribute('data-menu');
+});
